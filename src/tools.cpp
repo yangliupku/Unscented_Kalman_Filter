51d84e5d72c4ee8c573a030e78234586bba3aee1
Yang Liu <aragon@MacBook-Pro-2016.local>
2017-09-24 16:59:17 -0400

    first draft, compile and run without error

--- src/tools.cpp
+++ src/tools.cpp
@@ -10,9 +10,35 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
-                              const vector<VectorXd> &ground_truth) {
-  /**
-  TODO:
-    * Calculate the RMSE here.
-  */
+                              const vector<VectorXd> &ground_truth)
+{
+
+  VectorXd rmse(4);
+  rmse << 0, 0, 0, 0;
+
+  if (estimations.size() != ground_truth.size() || estimations.size() == 0)
+  {
+    cout << "Invalid estimation or ground_truth data" << endl;
+    return rmse;
+  }
+
+  //accumulate squared residuals
+  for (unsigned int i = 0; i < estimations.size(); ++i)
+  {
+
+    VectorXd residual = estimations[i] - ground_truth[i];
+
+    //coefficient-wise multiplication
+    residual = residual.array() * residual.array();
+    rmse += residual;
+  }
+
+  //calculate the mean
+  rmse = rmse / estimations.size();
+
+  //calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  //return the result
+  return rmse;
 }
